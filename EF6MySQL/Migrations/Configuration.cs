af92e02b92f8af6cb3ed0b6f0046f39b776a1184
Mikael Chudinov <mikael.chudinov@datametrix.no>
2014-10-08 01:49:22 +0200

    Switch framework to 4.5. Add sql.

--- EF6MySQL/Migrations/Configuration.cs
+++ EF6MySQL/Migrations/Configuration.cs
@@ -1,9 +1,6 @@
 namespace EF6MySQL.Migrations
 {
-    using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<EF6MySQL.MyModelContext>
     {
