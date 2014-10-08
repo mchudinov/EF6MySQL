4db9f1e05c379cfad23fd50a66bc81d5453e6305
Mikael Chudinov <mikael.chudinov@datametrix.no>
2014-10-08 19:44:04 +0200

    Test Hg

--- EF6MySQL/Program.cs
+++ EF6MySQL/Program.cs
@@ -8,7 +8,7 @@ namespace EF6MySQL
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World");
 
             using (var db = new MyModelContext())
             {
